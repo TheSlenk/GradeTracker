d0bc781686312b82d62d0adc0e15147b6dbacf29
TheSlenk <stephenmamosian@gmail.com>
2023-05-09 19:10:22 -0400

    basic login stuff

--- server.js
+++ server.js
@@ -2,16 +2,45 @@ const PORT = 3000;
 const express = require('express');
 const app = express();
 const mongo = require('mongodb');
+const session = require('express-session');
 const mongoClient = mongo.MongoClient;
 let db;
 
+// USE
+app.use(session({
+    secret: "audbaiupwdbauidbawiudbddsjmbdnfyfeshbjshfbla",
+    saveUninitialized:true,
+    cookie: { username: undefined,
+              email: undefined},
+    resave: false
+}));
+
+
+// EXPRESS CONFIG
+app.set('views', 'view');
+app.set('view engine', 'pug');
+
+
+
+// GET
 app.get('/', (req,res, next) => {
     res.redirect('/home');
 });
 
 app.get('/reg', (req, res, next) => {
-    
+    res.status(200).render('signup');
 });
+
+
+// SENDING FILES
+app.get('/login.css', (req,res,next) => {
+    res.sendFile('style/login.css', {root: __dirname});
+});
+
+app.get('/signup.js', (req,res,next) => {
+    res.sendFile('scripts/signup.js', {root: __dirname});
+});
+
 
 
 
