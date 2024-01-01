2d404f5ec00d3269604984d220c9f24d29b00f43
SlenK <stephenmamosian@gmail.com>
2024-01-01 04:07:16 -0500

    GX-0007-Basic course functionality added.

---
+++ scripts/course.js
@@ -0,0 +1,63 @@
+const categories = document.getElementById('categories');
+const final = document.getElementById('finalgrade');
+let data;
+function init(initData) {
+    data = initData;
+    update();
+}
+
+function addCategory(id, name, grade, weight) {
+    categories.innerHTML += `<div id="cat${id}"> 
+                            <input id="name${id}" value="${name}" oninput="updateName(${id})">
+                            <input id="grade${id}" value="${grade}" oninput="updateGrade(${id})"> 
+                            <input id="weight${id}" value="${weight}" oninput="updateWeight(${id})"> 
+                            <button onclick="removeCategory(${id})">Remove</button>
+                            <hr>`;
+}
+
+function updateName(id) {
+    console.log(data.categories[id].name);
+    console.log(document.getElementById(`name${id}`).value);
+    data.categories[id].name = document.getElementById(`name${id}`).value;
+}
+
+function updateGrade(id) {
+    data.categories[id].grade = document.getElementById(`grade${id}`).value;
+    onlyUpdateFinal();
+}
+
+function updateWeight(id) {
+    data.categories[id].weight = document.getElementById(`weight${id}`).value;
+    onlyUpdateFinal();
+}
+
+function removeCategory(id) {
+    data.categories.splice(id, 1);
+    update();
+}
+
+function addNewCategory() {
+    data.categories.push({name: '', grade: 0, weight: 0});
+    update();
+}
+
+function onlyUpdateFinal() {
+    let finalGrade = 0;
+    for(let cat of data.categories) {
+        finalGrade += cat.weight * cat.grade / 100;
+    }
+    final.value = `%${finalGrade}`;
+}
+
+function update() {
+    let weightSum = 0;
+    let finalGrade = 0;
+    categories.innerHTML = '';
+    let id = 0;
+    for(let cat of data.categories) {
+        addCategory(id++, cat.name, cat.grade, cat.weight);
+        weightSum += cat.weight;
+        finalGrade += cat.weight * cat.grade / 100;
+    }
+    final.value = `%${finalGrade}`;
+}
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const mongo = require('mongodb');
 const session = require('express-session');
 const mongoClient = mongo.MongoClient;
 let db;
+let accounts;
+
+class course {
+    categories;
+
+};
 
 // USE
 app.use(session({
@@ -30,8 +36,26 @@ app.get('/', (req,res, next) => {
     res.redirect('/home');
 });
 
+app.get('/home', (req, res) => {
+    res.status(200).render('home');
+});
+
 app.get('/reg', (req, res, next) => {
     res.status(200).render('signup');
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    res.status(200).render('course', {"data": JSON.stringify({"categories": [{'name': 'meow', 'grade': 100, 'weight': 5}, {'name': 'dog', 'grade': 54, 'weight': 65}]})});
+    return;
+    accounts.find({'id': id}).toArray((err, result) => {
+        if(err) throw err;
+        
+        if(result.length == 0)
+            res.status(404).send('Course Not Found!');
+        else
+            res.status(200).render('course', {"data": result[0]});
+    });
 });
 
 
@@ -40,6 +64,7 @@ mongoClient.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true}, (err, 
     if(err) throw err;
     
     db = client.db('GradeTracker');
+    accounts = db.collection('accounts');
     console.log('Connected to Database!');
 
     app.listen(PORT);
